3779dd8f6d13a0a54f0b5a2dbef801c4ea6f753a
sda607 <sda607@naver.com>
2022-07-27 17:29:03 +0900

    MVC

---
+++ src/main/java/com/mvc/MvcServlet.java
@@ -0,0 +1,50 @@
+package com.mvc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MvcServlet
+ */
+@WebServlet("/mvc01")
+public class MvcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//배열 자료
+		String[] name = {"김산", "이강", "정들"};
+		
+		//ArrayList 자료
+		ArrayList<Integer> lotto = new ArrayList<>();
+		lotto.add(16);
+		lotto.add(77);
+		lotto.add(2);
+		lotto.add(14);
+		lotto.add(12);
+		lotto.add(12);
+		
+		//model
+		request.setAttribute("names", name); //jsp쪽으로 names를 보냄
+		
+		request.setAttribute("lottoList", lotto);// jsp -> lottoList
+		
+		//포워딩
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("/mvc/mvc01.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
